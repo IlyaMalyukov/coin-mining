2ca29b4f074e35269e9bfa74c71161c87a978d83
imalyukov <imalyukov@phoenixit.ru>
2024-08-23 15:49:31 +0300

    app style

--- src/app/ui.vue
+++ src/app/ui.vue
@@ -4,11 +4,10 @@
 
 <style>
 #app {
-  width: 100%;
-  height: 100%;
-  min-height: 100vh;
+  width: 100vw;
+  height: 100vh;
   display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+  justify-content: center;
+  align-items: center;
 }
 </style>
